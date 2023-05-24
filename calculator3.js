68e2bc5a416e901f0da046a8a18fdcfbb02085dd
ValdsonSilva <valdsonmacedo15@gmail.com>
2023-05-23 23:56:41 -0300

    first commit

---
+++ calculator3.js
@@ -0,0 +1,5 @@
+function displaynum(n1){
+    //através da propriedade "form" podemos acessar qualquer elemento
+    //filho da tag "form" através do nome desse elemento
+    calculator.display.value = calculator.display.value + n1;
+};
